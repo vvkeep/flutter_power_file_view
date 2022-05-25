c225f9ed44a17b7a6dd64c68cc7abe1d5f1804b2
yaow <yaowei@ly-sky.com>
2022-05-25 16:37:57 +0800

    fix

---
+++ lib/src/enum/perview_type.dart
@@ -0,0 +1,23 @@
+/// PerviewType
+enum PerviewType {
+  /// Not initialized
+  none,
+
+  /// Unsupported platform
+  unsupportedPlatform,
+
+  /// Nonexistent file
+  nonExistent,
+
+  /// Unsupported file type
+  unsupportedType,
+
+  /// X5 initializing
+  engineLoading,
+
+  /// X5 Initialization failed
+  engineFail,
+
+  /// Successfully opened file
+  done
+}
