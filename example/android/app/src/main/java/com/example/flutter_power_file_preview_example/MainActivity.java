704776f6836842d5f2a8146edf3befc799aa52ce
yaow <yaowei@ly-sky.com>
2022-05-26 09:33:54 +0800

    clear project

--- example/android/app/src/main/java/com/example/flutter_power_file_preview_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_power_file_preview_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/flutter_power_file_preview.dart
+++
@@ -1,6 +0,0 @@
-library flutter_power_file_preview;
-
-export 'src/i18n/power_localizations.dart';
-export 'src/i18n/power_localizations_delegate.dart';
-export 'src/flutter_power_file_preview.dart';
-export 'src/widget/power_file_preview_widget.dart';
