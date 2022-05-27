4422b9c9bc8594662301eacc88100fd17b339390
cai <caily5@163.com>
2022-05-27 16:24:37 +0800

    fix

--- android/src/main/java/cn/vvkeep/power_file_view/PowerFileView.java
+++ android/src/main/java/cn/vvkeep/power_file_view/PowerFileView.java
@@ -1,11 +1,13 @@
 package cn.vvkeep.power_file_view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.tencent.smtt.sdk.TbsReaderView;
 
@@ -19,56 +21,42 @@ import io.flutter.plugin.platform.PlatformView;
 
 public class PowerFileView implements PlatformView {
 
-
-    private MethodChannel methodChannel;
-    private TbsReaderView readerView;
-    private FrameLayout frameLayout;
-    private String tempPath;
-    private String filePath;
-    private String fileType;
-    private PowerFileViewPlugin plugin;
+    private  TbsReaderView readerView;
+    private final FrameLayout frameLayout;
+    private final String tempPath;
+    private final String filePath;
 
     public PowerFileView(Context context,
                          BinaryMessenger messenger,
                          int id,
                          Map<String, Object> params,
                          PowerFileViewPlugin plugin) {
-        this.plugin = plugin;
-        //这里的Context需要Activity
+        // The Context here requires Activity  这里的Context需要Activity
         tempPath = context.getCacheDir().toString() + File.separator + "TbsReaderTemp";
         frameLayout = new FrameLayout(context);
         readerView = new TbsReaderView(context, new TbsReaderView.ReaderCallback() {
             @Override
             public void onCallBackAction(Integer integer, Object o, Object o1) {
-                LogUtils.e("TAG", "文件打开回调:" + integer + " " + o.toString() + " " + o1.toString());
             }
         });
         frameLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         readerView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         frameLayout.addView(readerView);
-        methodChannel = new MethodChannel(messenger, PowerFileViewPlugin.channelName + "_" + id);
         filePath = (String) params.get("filePath");
-        fileType = (String) params.get("fileType");
-        LogUtils.e("initFile", "filePath:" + filePath);
-        loadFile();
-    }
-
-    private void loadFile() {
-
         openFile();
     }
 
     private void openFile() {
         if (isSupportFile(filePath)) {
-            LogUtils.e("openFile", "打开文件中");
+            LogUtils.e("openFile");
+            //Fix the problem that no TbsReaderTemp folder exists causing the file to fail to load
             //增加下面一句解决没有TbsReaderTemp文件夹存在导致加载文件失败
             File bsReaderTempFile = new File(tempPath);
             if (!bsReaderTempFile.exists()) {
                 boolean isSuccess = bsReaderTempFile.mkdir();
             }
-            LogUtils.e("tempPath", "tempPath:" + tempPath);
-            LogUtils.e("filePath", "filePath:" + filePath);
-            //加载文件
+
+            // load file 加载文件
             Bundle localBundle = new Bundle();
             localBundle.putString("filePath", filePath);
             localBundle.putString("tempPath", tempPath);
